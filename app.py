466afaa8ad2dae8f3a00f2878db7314609710a4f
Ivan Lin <ivlin112@gmail.com>
2015-11-19 12:21:32 -0500

    Initial html

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def home():
-    return "MAIN PAGE"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.debug = True;
